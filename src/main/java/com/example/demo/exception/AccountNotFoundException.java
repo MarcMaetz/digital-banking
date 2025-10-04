93ea54018aa5dcf42d6474ac79b2c45eabe90639
Marc Maetz <marc.maetz@proton.me>
2025-10-04 15:12:12 +0200

    remove unused functions

--- src/main/java/com/example/demo/exception/AccountNotFoundException.java
+++ src/main/java/com/example/demo/exception/AccountNotFoundException.java
@@ -5,8 +5,5 @@ public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException(String message) {
         super(message);
     }
-    
-    public AccountNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
+
 }
